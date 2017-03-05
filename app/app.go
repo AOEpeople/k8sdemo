91d5666aede61087067944cdc1ba4c082cd0b28f
Bastian Ike <bastian.ike@aoe.com>
2017-03-05 10:43:26 +0100

    kubernetes config

--- app/app.go
+++ app/app.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"html/template"
+	"math/rand"
 	"net/http"
+	"os"
+	"time"
 )
 
 type (
@@ -13,6 +16,11 @@ type (
 	}
 )
 
+var (
+	health bool = true
+	me     int64
+)
+
 const tpl = `<!doctype html>
 <html>
 <head>
@@ -21,7 +29,8 @@ const tpl = `<!doctype html>
 </head>
 
 <body>
-    You are number <strong>{{.}}</strong>
+    Me: <strong>{{ .Me }}</strong><br/>
+    You are number <strong>{{ .Count }}</strong>
 </body>
 </html>
 `
@@ -37,18 +46,54 @@ func IndexController(backend CounterBackend) http.HandlerFunc {
 	return func(rw http.ResponseWriter, req *http.Request) {
 		backend.AtomicIncCounter()
 
-		tplInstance.Execute(rw, backend.GetCounter())
+		tplInstance.Execute(
+			rw,
+			struct {
+				Me    int64
+				Count uint64
+			}{
+				Me:    me,
+				Count: backend.GetCounter(),
+			},
+		)
+	}
+}
+
+func SleepController(rw http.ResponseWriter, req *http.Request) {
+	time.Sleep(10 * time.Second)
+}
+
+func PanicController(rw http.ResponseWriter, req *http.Request) {
+	os.Exit(1)
+}
+
+func ScaleController(rw http.ResponseWriter, req *http.Request) {
+	health = false
+}
+
+func HealthController(rw http.ResponseWriter, req *http.Request) {
+	if health {
+		rw.Write([]byte("ok"))
+	} else {
+		rw.WriteHeader(500)
 	}
 }
 
 func main() {
 	var backend CounterBackend = new(MemoryCounter)
 
+	me = rand.Int63()
+
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.HandleFunc(
 		"/style.css",
 		func(rw http.ResponseWriter, req *http.Request) { http.ServeFile(rw, req, "frontend/dist/style.css") },
 	)
+
+	http.HandleFunc("/health", HealthController)
+	http.HandleFunc("/sleep", SleepController)
+	http.HandleFunc("/panic", PanicController)
+	http.HandleFunc("/scale", ScaleController)
 	http.HandleFunc("/", IndexController(backend))
 
 	http.ListenAndServe(":3333", nil)
